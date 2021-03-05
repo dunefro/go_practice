c4b7eee6e3e1f3f8047861849ed03806cd6ae25a
Vedant Pareek <dunefro@gmail.com>
2021-03-05 14:36:03 +0530

    Added wrong code

---
+++ section4/understand_fmt/hello.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	n, err := fmt.Println("Hello")
+	fmt.Println(n)
+	fmt.Println(err)
+}
---
+++ section4/understand_fmt/wrong.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	n, e := fmt.Println("Hello")
+	fmt.Println(n)
+}
