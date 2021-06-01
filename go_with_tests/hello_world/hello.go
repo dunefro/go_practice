9c9cf6aa40237d5ba3a1b4c971cf41afac2f5e94
Vedant Pareek <dunefro@gmail.com>
2021-06-01 22:26:06 +0530

    Added constant

--- go_with_tests/hello_world/hello.go
+++ go_with_tests/hello_world/hello.go
@@ -2,8 +2,11 @@ package main
 
 import "fmt"
 
+// const is introduced so as to reduce creating the "hello " string again and again
+const englishHello = "Hello "
+
 func hello(name string) string {
-	return "Hello " + name + " !!"
+	return englishHello + name + " !!"
 }
 
 func main() {
