b9f29b9c188b60e38abd60a97228db310000ed54
Vedant Pareek <dunefro@gmail.com>
2021-06-04 22:38:41 +0530

    Added assert balance for deposit

--- go_with_tests/pointers_errors/pointers_test.go
+++ go_with_tests/pointers_errors/pointers_test.go
@@ -7,14 +7,18 @@ type Wallet struct {
 }
 
 func TestWallet(t *testing.T) {
-	t.Run("Wallet Deposit", func(t *testing.T) {
-		wallet := Wallet{}
-		wallet.Deposit(Bitcoin(30))
+
+	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		t.Helper()
 		got := wallet.Balance()
-		want := Bitcoin(30)
 		if got != want {
 			t.Errorf("Expected: %s Recieved: %s", got, want)
 		}
+	}
+	t.Run("Wallet Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(30))
+		assertBalance(t, wallet, Bitcoin(30))
 	})
 	t.Run("Wallet Withdrawl", func(t *testing.T) {
 		// balance must be added
