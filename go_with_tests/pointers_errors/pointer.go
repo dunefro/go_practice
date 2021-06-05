443d4dd9f988d060f918066a4d7f9c64abd181eb
Vedant Pareek <dunefro@gmail.com>
2021-06-05 17:54:30 +0530

    Added errors

--- go_with_tests/pointers_errors/pointer.go
+++ go_with_tests/pointers_errors/pointer.go
@@ -1,6 +1,15 @@
 package bitcoin
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+var ErrInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
 
 type Bitcoin int
 
@@ -8,8 +17,12 @@ func (w *Wallet) Deposit(money Bitcoin) {
 	w.balance += money
 }
 
-func (w *Wallet) Withdrawl(money Bitcoin) {
+func (w *Wallet) Withdrawl(money Bitcoin) error {
+	if w.balance < money {
+		return ErrInsufficientFunds
+	}
 	w.balance -= money
+	return nil
 }
 
 func (w *Wallet) Balance() Bitcoin {
--- go_with_tests/pointers_errors/pointers_test.go
+++ go_with_tests/pointers_errors/pointers_test.go
@@ -2,10 +2,6 @@ package bitcoin
 
 import "testing"
 
-type Wallet struct {
-	balance Bitcoin
-}
-
 func TestWallet(t *testing.T) {
 
 	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
@@ -13,6 +9,15 @@ func TestWallet(t *testing.T) {
 		got := wallet.Balance()
 		if got != want {
 			t.Errorf("Expected: %s Recieved: %s", got, want)
+		}
+	}
+	assertError := func(t testing.TB, errGotString error, errWantString error) {
+		t.Helper()
+		if errGotString.Error() == errWantString.Error() {
+			t.Errorf("Error: %q", errWantString)
+		}
+		if errGotString == nil {
+			t.Fatal("Wanted an error but didn't get one")
 		}
 	}
 	t.Run("Wallet Deposit", func(t *testing.T) {
@@ -25,5 +30,12 @@ func TestWallet(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
 		wallet.Withdrawl(Bitcoin(10))
 		assertBalance(t, wallet, Bitcoin(10))
+	})
+	t.Run("Withdrawl more than amount", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		err := wallet.Withdrawl(Bitcoin(30))
+		// Here we are passing the starting balance only
+		assertBalance(t, wallet, Bitcoin(20))
+		assertError(t, err, ErrInsufficientFunds)
 	})
 }
