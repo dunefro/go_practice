d843e3d831e7fa5c8fcfcf4243dd34c43e2b3b7a
Vedant Pareek <dunefro@gmail.com>
2021-06-05 18:00:59 +0530

    Added few more assertions

--- go_with_tests/pointers_errors/pointers_test.go
+++ go_with_tests/pointers_errors/pointers_test.go
@@ -13,11 +13,17 @@ func TestWallet(t *testing.T) {
 	}
 	assertError := func(t testing.TB, errGotString error, errWantString error) {
 		t.Helper()
-		if errGotString.Error() == errWantString.Error() {
-			t.Errorf("Error: %q", errWantString)
-		}
 		if errGotString == nil {
 			t.Fatal("Wanted an error but didn't get one")
+		}
+		if errGotString.Error() != errWantString.Error() {
+			t.Errorf("Expected Error: %q, Recieved Error: %q", errWantString, errWantString)
+		}
+	}
+	assertNoError := func(t testing.TB, err error) {
+		t.Helper()
+		if err != nil {
+			t.Errorf("Wasn't expecting any error here but got %q", err.Error())
 		}
 	}
 	t.Run("Wallet Deposit", func(t *testing.T) {
@@ -28,8 +34,9 @@ func TestWallet(t *testing.T) {
 	t.Run("Wallet Withdrawl", func(t *testing.T) {
 		// balance must be added
 		wallet := Wallet{balance: Bitcoin(20)}
-		wallet.Withdrawl(Bitcoin(10))
+		err := wallet.Withdrawl(Bitcoin(10))
 		assertBalance(t, wallet, Bitcoin(10))
+		assertNoError(t, err)
 	})
 	t.Run("Withdrawl more than amount", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
