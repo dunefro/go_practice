3ab18b0a408291e7420c73fe03aff39f164215ef
Vedant Pareek <dunefro@gmail.com>
2021-06-05 23:32:05 +0530

    Added error when key is missing

--- go_with_tests/maps/dictionary.go
+++ go_with_tests/maps/dictionary.go
@@ -1,7 +1,13 @@
 package maps
 
+import "errors"
+
 type Dictionary map[string]string
 
-func Search(m Dictionary, key string) string {
-	return m[key]
+func (d Dictionary) Search(key string) (string, error) {
+	definition, ok := d[key]
+	if !ok {
+		return "", errors.New("Could not find the word you are looking for")
+	}
+	return definition, nil
 }
--- go_with_tests/maps/dictionary_test.go
+++ go_with_tests/maps/dictionary_test.go
@@ -4,9 +4,20 @@ import "testing"
 
 func TestDictionary(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
-	got := Search(dictionary, "test")
-	want := "this is just a test"
-	assertStrings(t, got, want)
+
+	t.Run("Known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
+		assertStrings(t, got, want)
+	})
+	t.Run("Unknown Word", func(t *testing.T) {
+		_, err := dictionary.Search("random")
+		want := "Could not find the word you are looking for"
+		if err == nil {
+			t.Fatal("Expected error here")
+		}
+		assertStrings(t, err.Error(), want)
+	})
 }
 
 func assertStrings(t testing.TB, got string, want string) {
