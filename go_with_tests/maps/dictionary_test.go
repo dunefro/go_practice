ea5a0ccda67a7e1f0be94b34e23650cc306ea4f7
Vedant Pareek <dunefro@gmail.com>
2021-06-05 23:01:50 +0530

    Helper function

--- go_with_tests/maps/dictionary_test.go
+++ go_with_tests/maps/dictionary_test.go
@@ -6,7 +6,11 @@ func TestDictionary(t *testing.T) {
 	dictionary := map[string]string{"test": "this is just a test"}
 	got := Search(dictionary, "test")
 	want := "this is just a test"
+	assertStrings(t, got, want)
+}
 
+func assertStrings(t testing.TB, got string, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("Expected: %q Received: %q", want, got)
 	}
