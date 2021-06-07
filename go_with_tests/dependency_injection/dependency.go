1658dd942ffb12511f556b9ab7f777b7216d9936
Vedant Pareek <dunefro@gmail.com>
2021-06-07 14:55:39 +0530

    Added http handler

--- go_with_tests/dependency_injection/dependency.go
+++ go_with_tests/dependency_injection/dependency.go
@@ -1,14 +1,20 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
 )
 
-func Greet(writer *bytes.Buffer, name string) {
+func Greet(writer io.Writer, name string) {
 	fmt.Fprintf(writer, "Hello %s", name)
 }
 
-// func main() {
-// 	Greet(os.Stdout, "Harry")
-// }
+func MyGreetHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "Vedant")
+}
+
+func main() {
+	log.Fatal(http.ListenAndServe(":5000", http.HandlerFunc(MyGreetHandler)))
+}
