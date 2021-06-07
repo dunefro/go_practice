d5e30b70dcac2bccb687bf71b70200f0b516f4eb
Vedant Pareek <dunefro@gmail.com>
2021-06-07 15:22:29 +0530

    Echo API

--- go_with_tests/dependency_injection/dependency.go
+++ go_with_tests/dependency_injection/dependency.go
@@ -9,16 +9,13 @@ import (
 )
 
 func Greet(writer io.Writer, name string) {
+	//Trims the first /
 	name = strings.Trim(name, "/")
 	fmt.Fprintf(writer, "Hello %s", name)
 }
 
 func MyGreetHandler(w http.ResponseWriter, r *http.Request) {
 	Greet(w, r.URL.Path)
-	// if r.URL.Path != "/" {
-	// 	Greet(w, "Vedant")
-	// }
-	// if r.URL.Path != "/world"
 }
 
 func main() {
