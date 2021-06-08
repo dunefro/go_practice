e511fe32c24cf2d9d0780ca1c5d5b6d31616f775
Vedant Pareek <dunefro@gmail.com>
2021-06-08 11:01:22 +0530

    Introduced sleep of 1 seconds

--- go_with_tests/mocking/mocking.go
+++ go_with_tests/mocking/mocking.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
 func Countdown(w io.Writer) {
 	for i := 3; i >= 1; i-- {
 		fmt.Fprint(w, i, "\n")
+		time.Sleep(1 * time.Second)
 	}
 	fmt.Fprint(w, "Go!")
 }
