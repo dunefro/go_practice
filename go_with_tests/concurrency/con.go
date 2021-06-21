2982bd1990224b9712b94ccce98369a8acdcc1aa
Vedant Pareek <dunefro@gmail.com>
2021-06-21 14:34:37 +0530

    Added channel

--- go_with_tests/concurrency/con.go
+++ go_with_tests/concurrency/con.go
@@ -1,18 +1,24 @@
 package concurrency
 
-import "time"
-
 type WebsiteChecker func(string) bool
+type result struct {
+	string
+	bool
+}
 
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultchan := make(chan result)
 
 	for _, url := range urls {
 		go func(u string) {
-			results[u] = wc(u)
+			resultchan <- result{u, wc(u)}
 		}(url)
 	}
-	time.Sleep(2 * time.Second)
 
+	for i := 0; i < len(urls); i++ {
+		r := <-resultchan
+		results[r.string] = r.bool
+	}
 	return results
 }
