74a73737a64ed84b859193805b6fd31a45c26b67
Vedant Pareek <dunefro@gmail.com>
2021-06-21 15:12:05 +0530

    Added sample httptest

--- go_with_tests/select/sel_test.go
+++ go_with_tests/select/sel_test.go
@@ -1,10 +1,26 @@
 package sel
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
 
 func TestRacer(t *testing.T) {
-	slowURL := "http://www.facebook.com"
-	fastURL := "http://www.quii.co.uk"
+	// We create a slow server which will wait for 20 milliseconds
+	// The purpose of below function is to create a sample http server with http://12.0.0.1:<some high value port>
+	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(20 * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	// Fast server will not wait
+	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
 
 	want := fastURL
 	got := Racer(slowURL, fastURL)
