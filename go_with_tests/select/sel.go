11b5396f02db2eef4981d9f414cdfb5cd76853d8
Vedant Pareek <dunefro@gmail.com>
2021-06-22 22:17:22 +0530

    Added select statement

--- go_with_tests/select/sel.go
+++ go_with_tests/select/sel.go
@@ -1,29 +1,23 @@
 package sel
 
 import (
-	"fmt"
 	"net/http"
-	"time"
 )
 
 func Racer(url1 string, url2 string) (winner string) {
-
-	time1 := measureDuration(url1)
-	time2 := measureDuration(url2)
-
-	fmt.Printf("%q: %d\n%q: %d\n", url1, time1, url2, time2)
-
-	if time1 > time2 {
+	select {
+	case <-ping(url1):
+		return url1
+	case <-ping(url2):
 		return url2
 	}
-
-	return url1
 }
-func measureDuration(url string) time.Duration {
 
-	start := time.Now()
-	http.Get(url)
-	duration := time.Since(start)
-
-	return duration
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
 }
