1ed38c06d62c976971296e5dab7a41da49dd3907
Vedant Pareek <dunefro@gmail.com>
2021-06-22 22:31:31 +0530

    Added 10 milliseconds time limit

--- go_with_tests/select/sel_test.go
+++ go_with_tests/select/sel_test.go
@@ -8,22 +8,40 @@ import (
 )
 
 func TestRacer(t *testing.T) {
+	t.Run("Fake server testing", func(t *testing.T) {
+		slowServer := returnfakeserver(20 * time.Millisecond)
+		fastServer := returnfakeserver(0 * time.Millisecond)
 
-	slowServer := returnfakeserver(20 * time.Millisecond)
-	fastServer := returnfakeserver(0 * time.Millisecond)
+		// These statements are invoked here but executed once TestRacer completes
+		defer slowServer.Close()
+		defer fastServer.Close()
 
-	// These statements are invoked here but executed once TestRacer completes
-	defer slowServer.Close()
-	defer fastServer.Close()
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
 
-	slowURL := slowServer.URL
-	fastURL := fastServer.URL
+		want := fastURL
+		got, _ := Racer(slowURL, fastURL)
+		if want != got {
+			t.Errorf("We expected %q to be fast but %q was faster", want, got)
+		}
+	})
+	t.Run("Time more than 10 seconds", func(t *testing.T) {
+		slowServer := returnfakeserver(20 * time.Millisecond)
+		fastServer := returnfakeserver(30 * time.Millisecond)
 
-	want := fastURL
-	got := Racer(slowURL, fastURL)
-	if want != got {
-		t.Errorf("We expected %q to be fast but %q was faster", want, got)
-	}
+		// These statements are invoked here but executed once TestRacer completes
+		defer slowServer.Close()
+		defer fastServer.Close()
+
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
+
+		// want := fastURL
+		_, err := Racer(slowURL, fastURL)
+		if err == nil {
+			t.Errorf("We expected an error here")
+		}
+	})
 }
 
 func returnfakeserver(delay time.Duration) *httptest.Server {
