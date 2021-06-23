23e0ca8a7dc51a978d1dca40bc21a776bef81a29
Vedant Pareek <dunefro@gmail.com>
2021-06-23 16:11:00 +0530

    Refactored code

--- go_with_tests/reflection/refl.go
+++ go_with_tests/reflection/refl.go
@@ -6,19 +6,16 @@ import (
 
 func walk(x interface{}, fn func(input string)) {
 	val := getValue(x)
-	if val.Kind() == reflect.Slice {
+	switch val.Kind() {
+	case reflect.Slice:
 		for i := 0; i < val.Len(); i++ {
 			walk(val.Index(i).Interface(), fn)
 		}
-		return
-	}
-
-	for i := 0; i < val.NumField(); i++ {
-		field := val.Field(i)
-		switch field.Kind() {
-		case reflect.String:
-			fn(field.String())
-		case reflect.Struct:
+	case reflect.String:
+		fn(val.String())
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			field := val.Field(i)
 			walk(field.Interface(), fn)
 		}
 	}
