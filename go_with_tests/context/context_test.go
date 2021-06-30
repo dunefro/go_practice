0e1b5f998087c813c9bbb0e9afd1ad88f2224db7
Vedant Pareek <dunefro@gmail.com>
2021-06-30 13:49:10 +0530

    Added normal stub store

---
+++ go_with_tests/context/context_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+type StubStore struct {
+	response string
+}
+
+func (s *StubStore) Fetch() string {
+	return s.response
+}
+
+func TestServer(t *testing.T) {
+	data := "Hello world"
+	svr := Server(&StubStore{data})
+	request := httptest.NewRequest(http.MethodGet, "/", nil)
+	response := httptest.NewRecorder()
+	svr.ServeHTTP(response, request)
+	if response.Body.String() != data {
+		t.Errorf("Expected %s Received %s", data, response.Body.String())
+	}
+}
