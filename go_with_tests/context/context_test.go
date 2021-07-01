5ed45fe25c6e0afa7f86d4405e15f1a7c44da126
Vedant Pareek <dunefro@gmail.com>
2021-07-01 10:25:27 +0530

    Added test for cancel context

--- go_with_tests/context/context_test.go
+++ go_with_tests/context/context_test.go
@@ -1,26 +1,45 @@
 package main
 
 import (
+	"context"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 )
 
-type StubStore struct {
-	response string
+type SpyStore struct {
+	response  string
+	cancelled bool
 }
 
-func (s *StubStore) Fetch() string {
+func (s *SpyStore) Fetch() string {
+	time.Sleep(100 * time.Millisecond)
 	return s.response
 }
 
+func (s *SpyStore) Cancel() {
+	s.cancelled = true
+}
+
 func TestServer(t *testing.T) {
-	data := "Hello world"
-	svr := Server(&StubStore{data})
-	request := httptest.NewRequest(http.MethodGet, "/", nil)
-	response := httptest.NewRecorder()
-	svr.ServeHTTP(response, request)
-	if response.Body.String() != data {
-		t.Errorf("Expected %s Received %s", data, response.Body.String())
-	}
+	t.Run("tells store to cancel the work if request is cancelled", func(t *testing.T) {
+		data := "Hello world"
+		store := &SpyStore{
+			response: data,
+		}
+		svr := Server(store)
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+		cancellingCtx, cancel := context.WithCancel(request.Context())
+		time.AfterFunc(5*time.Millisecond, cancel)
+		request = request.WithContext(cancellingCtx)
+		response := httptest.NewRecorder()
+
+		svr.ServeHTTP(response, request)
+		if !store.cancelled {
+			t.Error("store was not told to cancel")
+		}
+
+	})
+
 }
