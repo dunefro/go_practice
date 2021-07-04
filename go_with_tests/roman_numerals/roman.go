e2b1c6010146d153ea3b2df771e3a23975e52ae1
Vedant Pareek <dunefro@gmail.com>
2021-07-04 22:37:04 +0530

    Added roman

--- go_with_tests/roman_numerals/roman.go
+++ go_with_tests/roman_numerals/roman.go
@@ -9,7 +9,7 @@ func ConvertToRoman(arabic int) string {
 
 	var result strings.Builder
 
-	romanmap := map[int]string{1: "I", 4: "IV", 5: "V", 9: "IX", 10: "X", 40: "XL", 50: "L", 90: "XC", 100: "C", 500: "D", 1000: "M"}
+	romanmap := map[int]string{1: "I", 4: "IV", 5: "V", 9: "IX", 10: "X", 40: "XL", 50: "L", 90: "XC", 100: "C", 400: "CD", 500: "D", 900: "CM", 1000: "M"}
 	no := []int{1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000}
 
 	for i := 0; i < len(no); i++ {
