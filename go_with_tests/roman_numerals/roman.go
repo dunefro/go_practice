7db9205ff9b83830b278f60f131b2b465ae85d26
Vedant Pareek <dunefro@gmail.com>
2021-07-04 22:35:13 +0530

    Added

--- go_with_tests/roman_numerals/roman.go
+++ go_with_tests/roman_numerals/roman.go
@@ -1,8 +1,30 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 func ConvertToRoman(arabic int) string {
-	if arabic == 2 {
-		return "II"
+
+	var result strings.Builder
+
+	romanmap := map[int]string{1: "I", 4: "IV", 5: "V", 9: "IX", 10: "X", 40: "XL", 50: "L", 90: "XC", 100: "C", 500: "D", 1000: "M"}
+	no := []int{1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000}
+
+	for i := 0; i < len(no); i++ {
+		if arabic == no[i] {
+			result.WriteString(romanmap[no[i]])
+			break
+		} else if no[i] > arabic {
+			fmt.Println(no[i], arabic)
+			if no[i]-arabic > no[i-1] {
+				result.WriteString(romanmap[no[i-1]] + ConvertToRoman(arabic-no[i-1]))
+			} else {
+				result.WriteString(ConvertToRoman(no[i]-arabic) + romanmap[no[i]])
+			}
+			break
+		}
 	}
-	return "I"
+	return result.String()
 }
--- go_with_tests/roman_numerals/roman_test.go
+++ go_with_tests/roman_numerals/roman_test.go
@@ -9,8 +9,11 @@ func TestConvertToRoman(t *testing.T) {
 		Arabic      int
 		Want        string
 	}{
-		{"Converting 1 to I", 1, "I"},
-		{"Converting 1 to II", 2, "II"},
+		// {"Converting 1 to I", 1, "I"},
+		// {"Converting 1 to II", 2, "II"},
+		// {"3 gets converted to III", 3, "III"},
+		// {"4 gets converted to IV (can't repeat more than 3 times)", 4, "IV"},
+		{"Converting 78", 78, "LXXVIII"},
 	}
 
 	for _, test := range cases {
