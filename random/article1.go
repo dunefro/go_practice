1a705e7ff534872fb9cf54c28b31d1c970264aa5
Vedant Pareek <dunefro@gmail.com>
2021-07-30 10:47:02 +0530

    The first Name doesn't change

--- random/article1.go
+++ random/article1.go
@@ -8,10 +8,24 @@ type person struct {
 }
 
 func main() {
-	Peter := person{"Vedant", "Pareek"}
-	fmt.Println(Peter.fullName())
+	Person := person{"Vedant", "Pareek"}
+	fmt.Println(Person.fullName())
+	fmt.Println("After changing the firstName")
+	Person.changeFirstName("Mr. Vedant")
+	fmt.Println(Person.fullName())
+	PointerPerson := &Person
+	PointerPerson.changeFirstName("Mr Vedant")
+	fmt.Println(Person.fullName())
 }
 
 func (p person) fullName() string {
 	return p.firstName + " " + p.lastName
+}
+
+func (p person) changeFirstName(s string) {
+	p.firstName = s
+}
+
+func (p *person) changeFirstName(s string) {
+	p.firstName = s
 }
