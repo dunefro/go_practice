fc4f460c3c2cd31089ec41cec2d328a852949986
Vedant Pareek <dunefro@gmail.com>
2021-07-30 10:50:27 +0530

    We observed that the value got updated

--- random/article1.go
+++ random/article1.go
@@ -11,8 +11,8 @@ func main() {
 	Person := person{"Vedant", "Pareek"}
 	fmt.Println(Person.fullName())
 	fmt.Println("After changing the firstName")
-	Person.changeFirstName("Mr. Vedant")
-	fmt.Println(Person.fullName())
+	// Person.changeFirstName("Mr. Vedant")
+	// fmt.Println(Person.fullName())
 	PointerPerson := &Person
 	PointerPerson.changeFirstName("Mr Vedant")
 	fmt.Println(Person.fullName())
@@ -22,9 +22,9 @@ func (p person) fullName() string {
 	return p.firstName + " " + p.lastName
 }
 
-func (p person) changeFirstName(s string) {
-	p.firstName = s
-}
+// func (p person) changeFirstName(s string) {
+// 	p.firstName = s
+// }
 
 func (p *person) changeFirstName(s string) {
 	p.firstName = s
