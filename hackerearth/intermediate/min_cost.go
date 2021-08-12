ba617cca84aa36f48f374b6355b746622c51a52d
Vedant Pareek <dunefro@gmail.com>
2021-08-13 00:37:13 +0530

    Improvised the logic

--- hackerearth/intermediate/min_cost.go
+++ hackerearth/intermediate/min_cost.go
@@ -14,27 +14,33 @@ func main() {
 	for i := 0; i < size; i++ {
 		fmt.Scanf("%d", &arr[i])
 	}
+	cost := 0
 	for i := 0; i < size; i++ {
 		if arr[i] > 0 {
 			rem := arr[i]
-			fmt.Println("__________________", arr[i])
+			// fmt.Println("__________________", arr[i])
 			for j := i + 1; j <= i+k; j++ {
-				fmt.Println(arr[j])
-				fmt.Println(rem)
+				// fmt.Println(arr[j])
+				// fmt.Println(rem)
 				if arr[j] < 0 && rem > 0 {
-					fmt.Println("Entered sinde")
-					if rem >= int(math.Abs(float64(arr[j]))) {
-						tarr[i] = tarr[i] - int(math.Abs(float64(arr[j])))
-						tarr[j] = tarr[j] + int(math.Abs(float64(arr[j])))
-						rem = rem - int(math.Abs(float64(arr[j])))
+					// fmt.Println("Entered sinde")
+					absValue := int(math.Abs(float64(arr[j] + tarr[j])))
+					if rem < absValue {
+						absValue = rem
 					}
+					tarr[i] = tarr[i] - absValue
+					tarr[j] = tarr[j] + absValue
+					rem = rem - absValue
 				}
 			}
 		}
-	}
-	cost := 0
-	for i := 0; i < size; i++ {
 		cost = cost + int(math.Abs(float64(arr[i]+tarr[i])))
 	}
+	// fmt.Println(arr)
+	// fmt.Println(tarr)
+	// cost := 0
+	// for i := 0; i < size; i++ {
+	// 	cost = cost + int(math.Abs(float64(arr[i]+tarr[i])))
+	// }
 	fmt.Println(cost)
 }
