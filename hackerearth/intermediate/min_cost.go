df5ebb8614d1d07ca489e1d2792d2913bc081ec1
Vedant Pareek <dunefro@gmail.com>
2021-08-13 00:23:14 +0530

    Working for sample test cases

--- hackerearth/intermediate/min_cost.go
+++ hackerearth/intermediate/min_cost.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	var size, k int
@@ -11,18 +14,27 @@ func main() {
 	for i := 0; i < size; i++ {
 		fmt.Scanf("%d", &arr[i])
 	}
-	for i:=0; i< size; i++ {
+	for i := 0; i < size; i++ {
 		if arr[i] > 0 {
 			rem := arr[i]
-			for j:=i+1; j<=i+k ; j++ {
+			fmt.Println("__________________", arr[i])
+			for j := i + 1; j <= i+k; j++ {
+				fmt.Println(arr[j])
+				fmt.Println(rem)
 				if arr[j] < 0 && rem > 0 {
-					if tarr[i]
-					tarr[i] = tarr[i] - math.Abs(arr[j]
-					tarr[j] = tarr[j] + math.Abs(arr[j])
-					rem = rem + arr[j]
+					fmt.Println("Entered sinde")
+					if rem >= int(math.Abs(float64(arr[j]))) {
+						tarr[i] = tarr[i] - int(math.Abs(float64(arr[j])))
+						tarr[j] = tarr[j] + int(math.Abs(float64(arr[j])))
+						rem = rem - int(math.Abs(float64(arr[j])))
+					}
 				}
 			}
 		}
 	}
-
+	cost := 0
+	for i := 0; i < size; i++ {
+		cost = cost + int(math.Abs(float64(arr[i]+tarr[i])))
+	}
+	fmt.Println(cost)
 }
