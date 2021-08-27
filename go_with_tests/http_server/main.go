82c6f47edee8e74e8caffbee37ea1d534bcbd6f2
Vedant Pareek <dunefro@gmail.com>
2021-08-27 09:22:13 +0530

    Added response header

--- go_with_tests/http_server/main.go
+++ go_with_tests/http_server/main.go
@@ -28,7 +28,11 @@ func main() {
 }
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	player := strings.TrimPrefix(r.URL.Path, "/players/")
-	fmt.Fprint(w, p.store.GetPlayerScore(player))
+	score := p.store.GetPlayerScore(player)
+	if score == 0 {
+		w.WriteHeader(http.StatusNotFound)
+	}
+	fmt.Fprint(w, score)
 }
 func (s *StubPlayerStore) GetPlayerScore(player string) int {
 	score := s.scores[player]
