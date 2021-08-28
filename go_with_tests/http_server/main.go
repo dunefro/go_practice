f256bbc996112d1e5fa321d53a9ea05f8aa85058
Vedant Pareek <dunefro@gmail.com>
2021-08-28 12:59:33 +0530

    Basic POST request

--- go_with_tests/http_server/main.go
+++ go_with_tests/http_server/main.go
@@ -28,6 +28,10 @@ func main() {
 }
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	player := strings.TrimPrefix(r.URL.Path, "/players/")
+	if r.Method == http.MethodPost {
+		w.WriteHeader(http.StatusAccepted)
+		return
+	}
 	score := p.store.GetPlayerScore(player)
 	if score == 0 {
 		w.WriteHeader(http.StatusNotFound)
--- go_with_tests/http_server/server_test.go
+++ go_with_tests/http_server/server_test.go
@@ -40,6 +40,19 @@ func TestGETPlayers(t *testing.T) {
 	})
 }
 
+func TestStoreWins(t *testing.T) {
+	store := StubPlayerStore{
+		map[string]int{},
+	}
+	server := &PlayerServer{&store}
+	t.Run("POST request", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodPost, "/players/Pepper", nil)
+		response := httptest.NewRecorder()
+		server.ServeHTTP(response, request)
+		assertStatus(t, response.Code, http.StatusAccepted)
+	})
+}
+
 func newgetscorerequest(player string) *http.Request {
 	request, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/players/%s", player), nil)
 	return request
