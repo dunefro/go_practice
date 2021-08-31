3b7750c68e1ec41bea47078e8a48046109be8301
Vedant Pareek <dunefro@gmail.com>
2021-08-31 09:56:47 +0530

    Added packaging cupcakes

---
+++ codechef/packaging_cupcakes.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	T, _ := strconv.ParseInt(scanner.Text(), 10, 64)
+	for i := int64(0); i < T; i++ {
+		scanner.Scan()
+		n, _ := strconv.ParseInt(scanner.Text(), 10, 64)
+		fmt.Println(n/2 + 1)
+	}
+}
