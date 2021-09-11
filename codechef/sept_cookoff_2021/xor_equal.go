ad06a889e1aaf3becfa6e574d16ead78719c6831
Vedant Pareek <dunefro@gmail.com>
2021-09-11 10:26:25 +0530

    max number in the array

--- codechef/sept_cookoff_2021/xor_equal.go
+++ codechef/sept_cookoff_2021/xor_equal.go
@@ -14,47 +14,40 @@ func main() {
 	scanner.Scan()
 	T, _ := strconv.ParseInt(scanner.Text(), 10, 64)
 	for i := int64(0); i < T; i++ {
-		var N, X int
+		var N, X int64
 		scanner.Scan()
 		a1 := strings.Split(strings.TrimRight(scanner.Text(), " "), " ")
 		for k := 0; k < len(a1); k++ {
 			if k == 0 {
-				n, _ := strconv.ParseInt(a1[k], 10, 64)
-				N = int(n)
+				N, _ = strconv.ParseInt(a1[k], 10, 64)
 			} else {
-				x, _ := strconv.ParseInt(a1[k], 10, 64)
-				X = int(x)
+				X, _ = strconv.ParseInt(a1[k], 10, 64)
 			}
 		}
 		N = N + 1 - 1
 		scanner.Scan()
-		a2 := strings.Split(scanner.Text(), " ")
-		// fmt.Println(a2, N, X)
-		var checker = make(map[int]map[string]int)
+		a2 := strings.Split(strings.TrimRight(scanner.Text(), " "), " ")
+		var checker = make(map[int64]map[string]int)
 		for j := 0; j < len(a2); j++ {
-			p, _ := strconv.ParseInt(a2[j], 10, 64)
-			P := int(p)
-			up := uint(P)
-			ux := uint(X)
-			xorP := int(up ^ ux)
+			P, _ := strconv.ParseInt(a2[j], 10, 64)
+			var xorP int64
+			xorP = int64(P ^ X)
 			if len(checker[P]) == 0 {
 				checker[P] = map[string]int{}
-				checker[P]["countermain"] = 1
+				checker[P]["countermain"] = 0
 				checker[P]["counterxor"] = 0
-			} else {
-				checker[P]["countermain"] += 1
 			}
+			checker[P]["countermain"] += 1
 			if xorP != P {
 				if len(checker[xorP]) == 0 {
 					checker[xorP] = map[string]int{}
 					checker[xorP]["countermain"] = 0
-					checker[xorP]["counterxor"] = 1
-				} else {
-					checker[xorP]["counterxor"] += 1
+					checker[xorP]["counterxor"] = 0
 				}
+				checker[xorP]["counterxor"] += 1
 			}
 		}
-		var maxval int
+		var maxval = -1
 		var minop = int(math.MaxInt64)
 		for _, v := range checker {
 			if v["countermain"]+v["counterxor"] > maxval {
