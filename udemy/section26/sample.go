c31d4a8da0c5b3ba2c87f7aec88fb80db5b7453f
Vedant Pareek <dunefro@gmail.com>
2021-11-08 02:18:35 +0530

    go doc and godoc

---
+++ udemy/section26/sample.go
@@ -0,0 +1,13 @@
+// Program to calculate the Sum of two integers
+package mymath
+
+import "fmt"
+
+func main() {
+	fmt.Println(Add(3, 4))
+}
+
+// Here we are adding two numbers
+func Add(a, b int) int {
+	return a + b
+}
