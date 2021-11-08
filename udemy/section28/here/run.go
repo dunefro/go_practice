c62f8c9a1d5d058fb05e93f578e1674166fdb33c
Vedant Pareek <dunefro@gmail.com>
2021-11-08 17:26:07 +0530

    Benchmarking example and testing

---
+++ udemy/section28/here/run.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"test/summing"
+)
+
+func main() {
+	fmt.Println(summing.Summing(1, 2, 3, 4))
+}
--- udemy/section28/main_test.go
+++ udemy/section28/main_test.go
@@ -4,8 +4,29 @@ import "testing"
 
 func TestMysum(t *testing.T) {
 	s := mySum(2, 3)
-	expected := 6
+	expected := 5
 	if s != expected {
 		t.Error("Expected", expected, "Got", s)
+	}
+}
+
+func TestTableAll(t *testing.T) {
+	type test struct {
+		data []int
+		sum  int
+	}
+	tests := []test{
+		test{[]int{2, 3}, 5},
+		test{[]int{1, 2, 3, 4, 5, 6, 7}, 28},
+		test{[]int{-1, 0, 1}, 0},
+		test{[]int{1, 3, 5, 7}, 16},
+		test{[]int{-10, -9, -8, 4}, -23},
+	}
+	for _, v := range tests {
+		expected := v.sum
+		got := mySum(v.data...)
+		if expected != got {
+			t.Error("Expected", expected, "got", got)
+		}
 	}
 }
