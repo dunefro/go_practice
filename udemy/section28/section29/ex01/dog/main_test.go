b546b14469377e13133c3b27e44200d78e229936
Vedant Pareek <dunefro@gmail.com>
2021-11-08 22:12:38 +0530

    Added two tests

--- udemy/section28/section29/ex01/dog/main_test.go
+++ udemy/section28/section29/ex01/dog/main_test.go
@@ -10,3 +10,13 @@ func TestYears(t *testing.T) {
 		t.Error("Expected", expected, "Got", got)
 	}
 }
+
+func TestYearsTwo(t *testing.T) {
+	dogAge := 5
+	got := YearsTwo(dogAge)
+	expected := 35
+
+	if got != expected {
+		t.Error("Expected", expected, "Got", got)
+	}
+}
