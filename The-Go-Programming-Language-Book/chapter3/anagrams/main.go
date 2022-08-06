b78a72be7575c9a00d18a69c823ed6f3457d2416
Vedant Pareek <dunefro@gmail.com>
2022-08-06 12:59:45 +0530

    Aangrams

---
+++ The-Go-Programming-Language-Book/chapter3/anagrams/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Print("Enter your first word: ")
+	scan1 := bufio.NewScanner(os.Stdin)
+	scan1.Scan()
+	text1 := scan1.Text()
+	fmt.Print("Enter your second word: ")
+	scan1.Scan()
+	text2 := scan1.Text()
+	fmt.Println(text1 == text2)
+	m1 := getMap(text1)
+	m2 := getMap(text2)
+	fmt.Println(m1)
+	fmt.Println(m2)
+	for b, _ := range m1 {
+		if m1[b] != m2[b] {
+			fmt.Println("Not anagrams")
+			return
+		}
+	}
+	fmt.Println("anagrams")
+}
+func getMap(s string) map[byte]int {
+	m := make(map[byte]int)
+	b := []byte(s)
+	for _, v := range b {
+		if v != ' ' {
+			m[v]++
+		}
+	}
+	return m
+}
