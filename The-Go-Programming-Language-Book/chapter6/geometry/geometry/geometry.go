e06216cdef7c240a10fd6591677b3806f6c8a1e4
Vedant Pareek <dunefro@gmail.com>
2022-08-22 08:50:33 +0530

    Geometry

--- The-Go-Programming-Language-Book/chapter6/geometry/geometry/geometry.go
+++ The-Go-Programming-Language-Book/chapter6/geometry/geometry/geometry.go
@@ -6,7 +6,19 @@ type Point struct {
 	X float64
 	Y float64
 }
+type Path []Point
 
 func (p Point) Distance(q Point) float64 {
 	return math.Hypot(q.X-p.X, p.Y-q.Y)
+}
+
+func (p Path) Distance() float64 {
+	var totalDistance float64
+	for i, q := range p {
+		if i == len(p)-1 {
+			totalDistance += q.Distance(p[0])
+		}
+		totalDistance += q.Distance(p[i+1])
+	}
+	return totalDistance
 }
