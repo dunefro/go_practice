3aac1838bac03219b12a3aef646a064051554f3a
Vedant Pareek <dunefro@gmail.com>
2022-08-22 09:22:30 +0530

    Code for hobbies

---
+++ The-Go-Programming-Language-Book/chapter6/nil_pointer_receiver/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+type Person struct {
+	name    string
+	age     int
+	hobbies []string
+}
+
+func main() {
+	p := &Person{
+		name: "A",
+		age:  12,
+		hobbies: []string{
+			"sports",
+			"youtube",
+			"music",
+		},
+	}
+	fmt.Println(p.message())
+	p.addHobbie("movies")
+	fmt.Println(p.message())
+
+}
+
+func (p *Person) message() string {
+	return fmt.Sprintf("My name is %s. I am %d years old. My hobbies are %s.", p.name, p.age, strings.Join(p.hobbies, ","))
+}
+
+func (p *Person) addHobbie(hobby string) {
+	p.hobbies = append(p.hobbies, hobby)
+}
